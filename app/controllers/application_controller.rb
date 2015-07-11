66054d1dfde41da29e9538a2b5e616e1669ea9a7
Leonardo Siqueira <leonardo.prg@gmail.com>
2015-07-10 23:47:15 -0300

    adding I18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   before_action :authenticate_user!
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = extract_locale_from_accept_language_header unless Rails.env.test?
+  end
+  
+  private
+
+  def extract_locale_from_accept_language_header
+    case request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    when 'pt'
+      'pt-BR'
+    else
+      'en'
+    end
+  end
+  
 end
