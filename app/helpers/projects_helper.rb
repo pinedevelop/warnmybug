9ef18ed6c0d2bcf50f2d834644da3c72373df702
Leonardo Siqueira <leonardo.prg@gmail.com>
2015-07-11 20:16:50 -0300

    fixing updated at at project card

---
+++ app/helpers/projects_helper.rb
@@ -0,0 +1,11 @@
+module ProjectsHelper
+  
+  def last_update_to(project)
+    if project.notifications.any?
+      l project.notifications.last.updated_at, format: :short
+    else
+      t('messages.never_used')
+    end
+  end
+  
+end
