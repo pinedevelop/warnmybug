6233544575266f12f949bc5b742cea96fc795d81
Leonardo Siqueira <leonardo.prg@gmail.com>
2015-07-12 03:03:41 -0300

    adding notification mailer spec

--- spec/mailers/notification_mailer_spec.rb
+++ spec/mailers/notification_mailer_spec.rb
@@ -1,5 +1,25 @@
 require "rails_helper"
 
 RSpec.describe NotificationMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe '#notify' do
+    let(:user) { create(:user) }
+    let(:notification) { create(:notification) }
+    let!(:browser_info) {create(:browser_info, notification_id: notification.id)}
+    
+    let(:mail) { NotificationMailer.notify(user, notification) }
+ 
+    it 'renders the subject' do
+      expect(mail.subject).to eql("WMB nova notificacão [#{notification.project.name}] [log]")
+    end
+ 
+    it 'renders the receiver email' do
+      expect(mail.to).to eql([user.email])
+    end
+ 
+    it 'assigns @email' do
+      expect(mail.body.encoded).to match(user.email)
+    end
+
+  end
 end
