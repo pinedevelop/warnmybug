0772d6aef595e2ecce277642c151add59dadda65
Leonardo Siqueira <leonardo.prg@gmail.com>
2015-07-12 02:23:45 -0300

    adding cors

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,15 @@ module Warnmybug
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.active_job.queue_adapter = :delayed_job
+    
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+        :headers => :any,
+        :methods => [:get, :put, :patch, :options]
+      end
+    end
+
   end
 end
