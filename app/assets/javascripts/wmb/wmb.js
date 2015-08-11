0ac92841eada5c03d60d2ae212d43ddc8c4c8516
Carlos Soares <carlos.soares17@gmail.com>
2015-08-11 22:38:20 +0100

    Added the option to have an queue of jobs with multiple workers

--- app/assets/javascripts/wmb/wmb.js
+++ app/assets/javascripts/wmb/wmb.js
@@ -1,5 +1,8 @@
-var wmb 				= {};
-wmb.url					= "https://warnmybug.com/api/notifications";
+var wmb 				     = {};
+wmb.url					     = "https://warnmybug.com/api/notifications";
+wmb.job_queue        = [];
+wmb.jobs_running     = 0;
+wmb.max_jobs_running = 2;
 
 console.log = function() {
 	wmb.notify('log', arguments);
@@ -23,24 +26,20 @@ wmb.track = function(uid) {
 
 wmb.notify = function(type, arguments) {
 	if(!wmb.uid) { return; }
-	var xhr = new XMLHttpRequest();
-	
-	xhr.open('POST', encodeURI(wmb.url));
-	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
 	var plugins = [];
-	
+
 	for(var i =0; i < navigator.plugins.length; i++) {
 		plugins.push(navigator.plugins[i]['name']);
 	};
-	
+
 	var mimeTypes = [];
-	
+
 	for(var i =0; i < navigator.mimeTypes.length; i++) {
 		mimeTypes.push(navigator.mimeTypes[i]['type']);
 	};
-	
-	var params = "notification[notification_type]=" + type +  
+
+	var params = "notification[notification_type]=" + type +
 							 "&notification[message]=" + arguments[0] +
 							 "&notification[url]=" + location.href +
 							 "&browser_info[app_code_name]=" + navigator.appCodeName +
@@ -58,7 +57,39 @@ wmb.notify = function(type, arguments) {
 							 "&browser_info[user_agent]=" + navigator.userAgent +
 							 "&browser_info[vendor]=" + navigator.vendor +
 							 "&browser_info[vendor_sub]=" + navigator.vendorSub +
+							 "&browser_info[referrer]=" + document.referrer +
 							 "&uid="  + wmb.uid;
-								
-	xhr.send(encodeURI(params));
+
+	 queueJob(params);
+}
+
+function queueJob (params) {
+	wmb.job_queue.push({ params: params });
+	if (wmb.jobs_running < wmb.max_jobs_running) {
+		wmb.jobs_running += 1; // Increment the process counter
+		startWorker();
+	}
+
+	function startWorker () {
+
+		makeRequest(wmb.job_queue.pop(), function () {
+			if (wmb.job_queue.length > 0) {
+				startWorker(); // Add an new job to the worker
+			} else {
+				wmb.jobs_running -= 1; // Remove the job from the process counter
+			}
+		});
+
+		function makeRequest (job, callback) {
+			var xhr = new XMLHttpRequest();
+
+			xhr.open('POST', encodeURI(wmb.url));
+			xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		 	// Add the callback
+			xhr.addEventListener("loadend", callback, false);
+			xhr.send(encodeURI(job.params));
+		}
+
+
+	}
 }
